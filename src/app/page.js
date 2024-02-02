1f2ddc12ef264598f89e762e082deaee06fd66f6
vivek <chimnanivivek14@gmail.com>
2024-02-02 14:21:53 +0530

    added Sidebar component to layout.js file

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,5 @@
-import Sidebar from "@/components/Sidebar";
 import Dashboard from "./dashboard/page";
 
 export default function Home() {
-  return (
-    <main className="flex w-screen h-screen">
-      <Sidebar />
-      <Dashboard />
-    </main>
-  );
+  return <Dashboard />;
 }
